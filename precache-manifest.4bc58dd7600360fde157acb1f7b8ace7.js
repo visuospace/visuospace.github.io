2f19abf95f6460e2ba33fc5b68ba4f7cc5b7b122
sayan banerjee <sankar69banerjee@gmail.com>
2020-06-14 12:20:55 +0530

    new

---
+++ precache-manifest.4bc58dd7600360fde157acb1f7b8ace7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2baa3b07a4c4a28bde05a32cd7463843",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6886b286ab730980017",
+    "url": "/static/css/main.10b8f09f.chunk.css"
+  },
+  {
+    "revision": "faeced64d6d1e256633b",
+    "url": "/static/js/2.04d3b4bf.chunk.js"
+  },
+  {
+    "revision": "f77df61ad5aee6f606510c5c8da8d179",
+    "url": "/static/js/2.04d3b4bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6886b286ab730980017",
+    "url": "/static/js/main.fd8d30aa.chunk.js"
+  },
+  {
+    "revision": "3b9cf721881a456d023c",
+    "url": "/static/js/runtime-main.6677f8c6.js"
+  },
+  {
+    "revision": "4ecee4f1bee28e53dfa1f936de67e175",
+    "url": "/static/media/logo.4ecee4f1.PNG"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/static/media/nucleo.f0b489a5.woff"
+  }
+]);
